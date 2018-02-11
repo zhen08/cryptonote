70760dbebe0e180b0167804cbdd108dec8003ff6
Zhen Liu <liuzhen@touchjet.com>
2018-02-12 12:29:40 +1300

    Clear P2P_STAT_TRUSTED_PUB_KEY.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -102,7 +102,7 @@ const uint32_t P2P_DEFAULT_CONNECTION_TIMEOUT                = 5000;          //
 const uint32_t P2P_DEFAULT_PING_CONNECTION_TIMEOUT           = 2000;          // 2 seconds
 const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; // 2 minutes
 const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          // 5 seconds
-const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
+const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8888888888888888888888888888888888888888888888888888888888888888";
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
