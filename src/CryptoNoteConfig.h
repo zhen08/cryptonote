3bd962cde522bff543d8f7411daafc5a23ebf473
Zhen Liu <zhen08@gmail.com>
2018-02-13 23:28:06 +0000

    Update genesis transaction.

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "gust";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001e61d029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210150bbd538da725f50b4c743f98ff1ab62f7db1806ed86007c0d2995f481230cc8";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff00002101b78dfbd707753a0e5dd431088312cb5504efd3050d44130f6aafecc736bc5154";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
